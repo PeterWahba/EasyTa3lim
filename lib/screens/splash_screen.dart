af8bf25ea75c32fbc5ca42cc4a0f1cc6c1acf1cd
Ahmed Saad Sharawi <ahmed.saad200253@gmail.com>
2024-08-19 02:28:08 +0300

    check device binding on splash

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -3,11 +3,16 @@
 import 'dart:convert';
 
 import 'package:academy_app/constants.dart';
+import 'package:academy_app/providers/auth.dart';
 import 'package:academy_app/screens/auth_screen_private.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_udid/flutter_udid.dart';
+import 'package:http/http.dart' as http;
+import 'package:mobile_device_identifier/mobile_device_identifier.dart';
+import 'package:provider/provider.dart';
+
 import '../providers/shared_pref_helper.dart';
 import 'tabs_screen.dart';
-import 'package:http/http.dart' as http;
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -34,6 +39,27 @@ class _SplashScreenState extends State<SplashScreen> {
     }
   }
 
+  Future<void> checkDeviceBinding() async {
+    String? authToken = await SharedPreferenceHelper().getAuthToken();
+
+    if (authToken == null || authToken.isEmpty) {
+      return;
+    }
+    final deviceId = base64.encode(utf8.encode((await MobileDeviceIdentifier().getDeviceId())!));
+    String udid = await FlutterUdid.consistentUdid;
+
+    var url = "$BASE_URL/api/device_binding?token=$authToken";
+    var response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      var data = json.decode(response.body);
+      // compare the device id and udid with the data from the server
+      if (data['deviceid'] != deviceId || data['udid'] != udid) {
+        // if the device id and udid are not the same, log out the user
+        await Provider.of<Auth>(context, listen: false).logout();
+      }
+    }
+  }
+
   @override
   void initState() {
     donLogin();
@@ -44,17 +70,15 @@ class _SplashScreenState extends State<SplashScreen> {
   void donLogin() {
     String? token;
     Future.delayed(const Duration(seconds: 3), () async {
+      await checkDeviceBinding();
       token = await SharedPreferenceHelper().getAuthToken();
       if (token != null && token!.isNotEmpty) {
-        Navigator.of(context).pushReplacement(
-            MaterialPageRoute(builder: (context) => const TabsScreen()));
+        Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => const TabsScreen()));
       } else {
         if (courseAccessibility == 'publicly') {
-          Navigator.of(context).pushReplacement(
-              MaterialPageRoute(builder: (context) => const TabsScreen()));
+          Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => const TabsScreen()));
         } else {
-          Navigator.of(context).pushReplacement(MaterialPageRoute(
-              builder: (context) => const AuthScreenPrivate()));
+          Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => const AuthScreenPrivate()));
         }
       }
     });
