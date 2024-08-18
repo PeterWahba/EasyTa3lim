39f34c93386a0fff281fe4c0ba014a42ac163c7a
Ahmed Saad Sharawi <ahmed.saad200253@gmail.com>
2024-08-18 22:22:41 +0300

    disable screenshots on android

--- android/app/src/main/kotlin/com/example/academy_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/academy_app/MainActivity.kt
@@ -1,6 +1,67 @@
 package com.ta3lim.academy_app
 
+import android.view.WindowManager.LayoutParams;
+import android.os.Bundle
+import androidx.annotation.NonNull
+import io.flutter.view.FlutterView
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
+import android.util.Log
+import android.view.SurfaceView
+import android.view.View
+import android.view.ViewGroup
+import android.view.WindowManager
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        window.addFlags(LayoutParams.FLAG_SECURE)
+        super.configureFlutterEngine(flutterEngine)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (!setSecureSurfaceView()) {
+            Log.e("MainActivity", "Could not secure the MainActivity!")
+            // React as appropriate.
+        }
+    }
+
+    private fun setSecureSurfaceView(): Boolean {
+        val content = findViewById<ViewGroup>(android.R.id.content)
+        if (!isNonEmptyContainer(content)) {
+            return false
+        }
+        val splashView = content.getChildAt(0)
+        if (!isNonEmptyContainer(splashView)) {
+            return false
+        }
+        val flutterView = (splashView as ViewGroup).getChildAt(0)
+        if (!isNonEmptyContainer(flutterView)) {
+            return false
+        }
+        val surfaceView = (flutterView as ViewGroup).getChildAt(0)
+        if (surfaceView !is SurfaceView) {
+            return false
+        }
+        surfaceView.setSecure(true)
+        this.window.setFlags(
+            WindowManager.LayoutParams.FLAG_SECURE,
+            WindowManager.LayoutParams.FLAG_SECURE
+        )
+        return true
+    }
+
+    private fun isNonEmptyContainer(view: View): Boolean {
+        if (view !is ViewGroup) {
+            return false
+        }
+        if (view.childCount < 1) {
+            return false
+        }
+        return true
+    }
 }
